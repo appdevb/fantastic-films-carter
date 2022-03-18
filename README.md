08594038cd5445421a03ceabb8ec441acbb976f2
appdevb <54305761+appdevb@users.noreply.github.com>
2022-03-18 16:11:51 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# react-moviedb-api
+
+   Fantastic Films Carter React Web App With TheMoviesDB API
+
+
+# Resources
+
+    ReactJs: https://reactjs.org/
+
+    API: https://www.themoviedb.org/
+
+    Heroku: https://heroku.com
+
+# Preview
+
+!["Fantastic Films Carter React Web App With TheMoviesDB API"](https://fantastic-films-carter.herokuapp.com/g "Fantastic Films Carter React Web App With TheMoviesDB API")
+
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './footer.scss';
+
+import { Link } from 'react-router-dom';
+
+import bg from '../../assets/footer-bg.jpg';
+import logo from '../../assets/tmovie.png';
+
+const Footer = () => {
+    return (
+        <div className="footer" style={{backgroundImage: `url(${bg})`}}>
+            <div className="footer__content container">
+                <div className="footer__content__logo">
+                    <div className="logo">
+                        <img src={logo} alt="" />
+                    </div>
+                </div>
+                <div className="footer__content__menus">
+                    <div className="footer__content__menu">
+                        <Link to="/">Home</Link>
+                    </div>
+                    <div className="footer__content__menu">
+                        <Link to="/">Popular</Link>
+                    </div>
+                    <div className="footer__content__menu">
+                        <Link to="/">About The Developer</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
